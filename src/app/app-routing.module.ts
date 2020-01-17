fc16b08b10c1350b5d9022e655837e62bdb75719
Suziane <C0605732@vale.com>
2020-01-17 07:59:41 -0300

    Start

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+export const routes: Routes = [
+  {
+      component: HomeComponent,
+      path: '',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
